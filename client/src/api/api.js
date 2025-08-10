190b297fed566c74f97eb65bd3938a7c3313bee5
CHACHA0044 <demblapranav@gmail.com>
2025-08-10 12:24:00 +0530

    api url addedfor backend

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const BASE = 'https://cft-be.onrender.com' || 'http://localhost:5000/api';
 const API = axios.create({
   baseURL: BASE,
   withCredentials: true,

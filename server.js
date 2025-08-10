cba340c94a56dac8b014c55f1fc0f857e97506a3
CHACHA0044 <demblapranav@gmail.com>
2025-08-10 16:13:04 +0530

    changes to server file

--- server.js
+++ server.js
@@ -115,9 +115,9 @@ app.listen(PORT, () => {
 
 // serve frontend in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
+

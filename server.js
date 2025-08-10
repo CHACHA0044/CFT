bf95268fa09a941872912241a4d1e3be52afc94d
CHACHA0044 <demblapranav@gmail.com>
2025-08-10 15:59:35 +0530

    qjndo

--- server.js
+++ server.js
@@ -114,10 +114,10 @@ app.listen(PORT, () => {
 });
 
 // serve frontend in production
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-//   });
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}

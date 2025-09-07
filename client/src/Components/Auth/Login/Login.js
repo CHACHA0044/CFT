5933e157774679146f1d3059926a6fceb16a182b
CHACHA0044 <demblapranav@gmail.com>
2025-09-07 23:21:14 +0530

    dd

--- client/src/Components/Auth/Login/Login.js
+++ client/src/Components/Auth/Login/Login.js
@@ -523,7 +523,7 @@ useEffect(() => {
               type="button"
               onClick={() => setShowPassword(!showPassword)}
               className="absolute right-9 md:right-12 top-[185px] md:top-[209px] transform -translate-y-1/2 text-gray-500 hover:text-black transition-colors duration-200 focus:outline-none"
-            >
+             >
               {showPassword ? (
                 <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.878 9.878L3 3m6.878 6.878L21 21" />

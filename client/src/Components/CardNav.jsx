c9cc242e1a6578834255c9133e1a73dfc699c02b
CHACHA0044 <demblapranav@gmail.com>
2025-09-17 18:10:45 +0530

    bm

--- client/src/Components/CardNav.jsx
+++ client/src/Components/CardNav.jsx
@@ -61,7 +61,7 @@ useEffect(() => {
             exit={{ x: `-${width}` }}
             transition={{ duration: 0.4, ease: [0.4, 0, 0.2, 1] }}
             style={{ width }}
-            className="absolute top-0 left-0 h-auto p-4 bg-white/20 dark:bg-gray-800/70 rounded-r-3xl backdrop-blur-md shadow-lg flex flex-col"
+            className="absolute top-0 -left-10 h-auto p-4 bg-white/20 dark:bg-gray-800/70 rounded-r-3xl backdrop-blur-md shadow-lg flex flex-col"
           >
             {/* Optional Title */}
 

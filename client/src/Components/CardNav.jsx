7fa12046617e5b7f55331948b7cf1c58c09d46f8
CHACHA0044 <demblapranav@gmail.com>
2025-10-05 01:18:17 +0530

    added nowrap

--- client/src/Components/CardNav.jsx
+++ client/src/Components/CardNav.jsx
@@ -83,7 +83,7 @@ const AnimatedHeadline = ({ sentence = "" }) => {
     return (
       <div className="relative overflow-visible w-full justify-center items-center">
         <motion.div
-          className=" md:text-3xl text-xl font-germania font-medium sm:font-semibold tracking-wide text-shadow-DEFAULT text-emerald-500 dark:text-white transition-colors duration-500"
+          className="whitespace-nowrap md:text-3xl text-xl font-germania font-medium sm:font-semibold tracking-wide text-shadow-DEFAULT text-emerald-500 dark:text-white transition-colors duration-500"
           initial="hidden"
           animate="visible"
           variants={{
